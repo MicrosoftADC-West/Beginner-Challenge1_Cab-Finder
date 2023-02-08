8370503a48d49d62a4e0372b0d724f8eec919eea
tdadadavid <davidtofunmidada@gmail.com>
2023-02-08 15:46:01 +0100

    modularize it

--- main.ts
+++ main.ts
@@ -5,21 +5,24 @@ import {
     getLocations, 
     getRideServices, 
     getRides 
-} from "./src";
+} from "./src/challenge1";
 
 
 
 async function main() {
+    // Challenge 1
     const rides = getRides();
     const ridesServices = getRideServices();
     const locations = getLocations();
     const priceForEachRide: RidePrice[][] = getPriceForEachRide(locations,ridesServices,rides);
     const bestRideForEachRoute = getBestRideForEachRoute(locations,ridesServices,rides)
     
-    return {
+    const challenge1 = {
         task1: priceForEachRide,
         task2: bestRideForEachRoute,
     }
+
+    // challenge 2
 }
 
 main().then(console.log);
