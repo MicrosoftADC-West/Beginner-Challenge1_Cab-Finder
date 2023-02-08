de6469a57fca95ca23d2c25722543f4589465c36
Olasunkanmi Fafowora <sunkanmifafowora@gmail.com>
2023-02-08 21:44:19 +0100

    Added start files for challenge 2 project

---
+++ olasunkanmi-fafowora/Challenge-2/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
