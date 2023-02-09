57cefea2a57d27bbce4d03f4734cc16654c73047
AishaBukar <49945216+AishaBukar@users.noreply.github.com>
2023-02-09 21:27:53 +0100

    get-ride and add-ride API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\RideController;
+
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +17,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::prefix("v1")->group(function () {
+    Route::get("/get-ride", [RideController::class, "getRides"]);
+    Route::post("/add-ride", [RideController::class, "addRide"]);
+   
 });
+
